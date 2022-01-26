dfc07280818e8bce80d37cbf2d8e56eb65fc2de7
josuepl <josue.perez.lucero@gmail.com>
2022-01-26 13:23:25 -0600

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -3,14 +3,24 @@
 #include <sys/wait.h>
 
 int main(){
-int x;
+int x, fd[2];
+char buffer[10];
 pid_t pa, pb;
 x = 4;
+pipe(fd);
 if((pa = fork()) == 0){// proceso Hijo A
-  printf("PA-> x;%d P:%d PP;%d ",x,getpid(),getppid());
+  buffer[0]= 'h';
+  buffer[1]= 'o';
+  buffer[2]= 'l';
+  buffer[3]= 'a';
+  write(fd[1],buffer,4);
+  printf("PA-> BUFFER:%s P:%d PP:%d \n",buffer,getpid(),getppid());
+ 
 }
 else{// Proceso Padre - Main
-  printf("PM-> x;%d P:%d PP;%d ",x,getpid(),getppid());
+  waitpid(pa,NULL,0);
+  read(fd[0],buffer,4);
+  printf("PM-> BUFFER:%s P:%d PP:%d \n",buffer,getpid(),getppid());
 }
 
 return 0;
