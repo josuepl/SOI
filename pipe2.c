ed1f9cf14ef1f88de7a6272f95e2f364861eca28
josuepl <josue.perez.lucero@gmail.com>
2022-03-25 13:44:23 -0600

    Update pipe2.c

--- pipe2.c
+++ pipe2.c
@@ -5,7 +5,8 @@
 int main(){
 int c11[2], c12[2], c21[2],c22[2];
 char matrizA[2][3], matrizB[2][3];
-char resultado[4];
+char resultado[5];
+pid_t h00,h01,h10,h11;
 
 // Tuberias
 pipe(c11);
@@ -28,8 +29,23 @@ matrizB[1][0]= 'h';
 matrizB[1][1]= 'i';
 matrizB[1][2]='\0';
 
-printf("matriz A: \n, %s, \n %s\n",matrizA[0], matrizA[1]);
-printf("matriz B: \n, %s, \n %s\n",matrizB[0], matrizB[1]);
+printf("matriz A: \n [%s] \n [%s]\n",matrizA[0], matrizA[1]);
+printf("matriz B: \n [%s] \n [%s]\n",matrizB[0], matrizB[1]);
 
+write(c11[1],matrizA[0],1);
+write(c11[1],matrizB[0],1);
+
+if((h00 = fork()) == 0){//hijo
+ char aux[2];
+ read(c11[0],aux,2);
+ printf("HIJO00: %s PID:%d, PPID:%d\n",aux,getpid(),getppid());
+ write(c11[1],aux,2);
+}
+else{// padre
+ sleep(3);
+ printf("PADRE: -----");
+ read(c11[0],resultado,2);
+ printf("Resultado: %s \n",resultado);
+}
 
 }
