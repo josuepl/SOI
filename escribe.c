e8a74ce2c6ac0d3b693229e1e807b249b01e5150
josuepl <josue.perez.lucero@gmail.com>
2022-06-21 17:58:33 -0500

    update

--- escribe.c
+++ escribe.c
@@ -8,10 +8,10 @@ int main(){
  char buffer[10];
  fd = open("datos.txt",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
  printf("Ingresa la cadena a almacenar: \n");
- //scanf("%*c%[^\n]",buffer);
+ scanf("%*c%[^\n]",buffer);
  //scanf("%s",buffer);
+ //fgets(buffer,10,stdin);
  printf("La cadena es: %s \n",buffer);
- fgets(buffer,10,stdin);
  //printf("La cadena es: %s \n",buffer);
  printf("fd: %d TamCad:%d \n",fd,sizeof(buffer));
  write(fd, &buffer, sizeof(buffer));
--- pipe.c
+++ pipe.c
@@ -9,19 +9,28 @@ int x, fd[2],i;
 // fd es descriptor de archivos -> referencia a un espacio o una direccion
 // dentro del SO fd[0]-> Leer, read || fd[1]-> Escribir, write
 char buffer[10];
-pid_t pa, pb;
+pid_t pa, pb, pa1;
 x = 4;
 pipe(fd);
 if((pa = fork()) == 0){// proceso Hijo A
+  x = 10 + x;
   buffer[0]= 'H';
   buffer[1]= 'O';
-  buffer[2]= 'L';
-  buffer[3]= 'A';
-  write(fd[1],buffer,4);
+  write(fd[1],buffer,2);
   printf("PA-> BUFFER:%s P:%d PP:%d \n",buffer,getpid(),getppid());
- 
+  if((pa1 = fork()) == 0){//NIETO
+   read(fd[0],buffer,2);
+   buffer[2]= 'L';
+   buffer[3]= 'A';
+   write(fd[1],buffer,4);
+  }
+  else{
+   waitpid(pa1,NULL,0);
+  }
+ //printf("PA ->El valor de x es :%d \n",x);
 }
 else{// Proceso Padre - Main
+ x = -8 + x;
   waitpid(pa,NULL,0);
   read(fd[0],buffer,4);
   char aux[4];
@@ -32,7 +41,11 @@ else{// Proceso Padre - Main
   printf("\n");
   printf("AUX:%s \n",aux);
   write(fd[1],aux,4);
+ //printf("PADRE ->El valor de x es :%d \n",x);
+
   if((pb = fork()) == 0 ){// Proceso Hijo B
+   x = x + 10;
+ 
    read(fd[0],buffer,4);
    buffer[4]= 'm';
    buffer[5]= 'u';
@@ -41,11 +54,14 @@ else{// Proceso Padre - Main
    buffer[8]= 'o';
    write(fd[1],buffer,9);
   
+ printf("PB ->El valor de x es :%d \n",x);
+
   }else{// Proceso Padre - Main
    waitpid(pb,NULL,0);
    read(fd[0],buffer,9);
    printf("PM-> BUFFER:%s P:%d PP:%d \n",buffer,getpid(),getppid());
- 
+   //printf("PADRE ->El valor de x es :%d \n",x);
+
   }
 }
 
